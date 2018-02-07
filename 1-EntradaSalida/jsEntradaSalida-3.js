7e952222c8727ad8d2b4248bcc64bedecd8dfcf7
vladimoreyra17 <36193862+vladimoreyra17@users.noreply.github.com>
2018-02-07 01:55:07 -0300

    concatenar

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,12 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+
+
+	nombre=document.getElementById('elNombre').value;
+	alert("su nombre es:  "+nombre);
+
 }
 
 
