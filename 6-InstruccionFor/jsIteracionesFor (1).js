0a4100c65828e496b8fbcf8a84aab30c0c213f8b
vladimoreyra17 <36193862+vladimoreyra17@users.noreply.github.com>
2018-02-21 10:50:52 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,55 @@
 function Mostrar()
 {
+	var iteraciones=10;
+	var contador=0;
+	var par=0;
+	var impar=0;
+	var cero=0;
+	var cantpositivo=0;
+	var cantnegativo=0;
+	//var iteraciones=10;
 
+	for(contador=-10 ;contador<iteraciones ; contador++ )
+	{		
+	document.write("<br>" +contador);
+		if(contador==0)	
+		{
+			cero++;			
+		}
+			
+		else
+		{
+		
+		if(contador%2==0)
+		{
+			par++;
+		}	
+		 
+		 else{
+		
+			impar++
+		}
+		if(contador>0)
+		{
+			cantpositivo++;
+		}
+
+			else
+			{
+				if(contador<0)
+				{
+					cantnegativo++;
+				}
+			}
+		}
+
+		}	
+
+			
+	}
+	document.write("<br> cero:" +cero);
+	document.write("<br> par: " +par);
+	document.write("<br> impar:" +impar);
+	document.write("<br> negativo:" +cantnegativo);
+	document.write("<br> positivo:" +catpositivo);
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,14 @@
 function Mostrar()
 {
+	var contador;
+	var iteraciones=0;
+
+	for(contador=10 ; contador>iteraciones ;contador-- )
+	{	//alert(""+contador);	
+	document.write("<br>" +contador);
+	}
+
+	document.write("<br>" +contador);
 
 
 }
