d3a27a885d2b6c85af40d489189a4928db765909
vladimoreyra17 <36193862+vladimoreyra17@users.noreply.github.com>
2018-04-11 11:18:09 -0300

    completar cadena string mas secuencial

---
+++ fgdsgfgf/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define CANT 3
+int main()
+{
+    int legajo[CANT];
+    char nombre[CANT][31];//cant--cant de nombres(columna) 31-- cant de elemento del nombr(fila)//
+
+    for(int i=0; i<CANT; i++)
+    {
+
+        printf("legajo");
+        legajo[i]=i+1;
+        printf("nombre");//----->&nombre[i[0]
+        fflush(stdin);
+        scanf("%[^\n]",nombre[i]);
+
+    }
+    for()
+    return 0;
+}
